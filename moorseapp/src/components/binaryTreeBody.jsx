c65a799290829ce0853a5a59df58f77b3711ddd0
Jallal ☁ <jhazzat@gmail.com>
2023-04-01 20:46:26 +0000

    [Jallal] added more refactoring

--- moorseapp/src/components/binaryTreeBody.jsx
+++ moorseapp/src/components/binaryTreeBody.jsx
@@ -12,6 +12,7 @@ class BinaryModule extends React.Component {
 <h2>What is Kubernetes?</h2>
 <p>Kubernetes is a platform for running container-based cloud-native applications. It offers different resources which allow for storing information in the cloud instead of a local host. Offers enreprise features like scalability and availability. It orchestrates containers such a way that they are providing the services that arer required in the envirnment where these services are requirted. The solution is based on the borg technology that good has been using for many years in their datacenters.</p>
 <br></br>
+<code-section>
 <p>Other container management Solutions includes: 
   <br></br>
 <ui>
@@ -21,6 +22,7 @@ class BinaryModule extends React.Component {
   <li>Google Anthos</li>
 </ui>
 </p>
+</code-section>
 <br></br>
 <h2>What are Containers?</h2>
 <p>A container image includes all dependecies required to run an application. Containers are running instances of container images. To run a conatiner, a container engine is required. Container engines run on top of host operatying system. Docker is a leading solution in containers, RedHat Podman is upcoming. Kubernetes adds cluster features to containers by managing them in pod resources. Kubernetes can be offewred through different host platforms such as a hosted service in public cloud, on top of a physical cluster (on premise), or as an all-in-one solution, running on Minikube. Clould Native Computing Fouindation (CNCF) is a governing body that solves issues faced by any cloud native application (so not just Kubernetes). Google donated Kubernetes to the Cloud Native Computing FOundation, which is a foundation in Linux Foundation. CNCF owns the copyright of Kubernetes. There sre many options such as minikube, cloud based, DYI AIO, or Docker Desktop.</p>
